c53a3d383314080a6af98a4b141b46fecef21a9f
Arhyel Ramos Flores <arthy89>
2023-07-16 02:20:40 -0500

    routing

--- resources/views/layouts/events/events-index.blade.php
+++ resources/views/layouts/events/events-index.blade.php
@@ -1,27 +1,17 @@
 @extends('layouts.app')
 
-@push('css')
-    {{-- css por añadir --}}
-@endpush
-
 @section('content')
-    {{-- CONTENIDO --}}
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header text-bg-success">{{ __('Bienvenido al sistema') }}</div>
+                    <div class="card-header text-bg-success">EVENTOOOOOOS</div>
 
                     <div class="card-body">
-                        Te damos la bienvenida fgiuyfiuy
+                        EVENTOS INDEX
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
-
-@push('scripts')
-    {{-- js por añadir --}}
-@endpush
--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,77 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
 
-Route::resource('tiposusuarios', TipoUsuarioController::class);
-Route::resource('incidencias', IncidenciaController::class);
-Route::resource('asistentes', AsistenteController::class);
-Route::resource('tiposeventos', TipoEventoController::class);
-Route::resource('acta', ActaController::class);
-Route::resource('memorandum', MemorandumController::class);
-Route::resource('evento', EventController::class);
+Route::resource('tiposusuarios', TipoUsuarioController::class)->names([
+    'index' => 'tiposusuarios.index',
+    'create' => 'tiposusuarios.create',
+    'store' => 'tiposusuarios.store',
+    'show' => 'tiposusuarios.show',
+    'edit' => 'tiposusuarios.edit',
+    'update' => 'tiposusuarios.update',
+    'destroy' => 'tiposusuarios.destroy',
+]);
+
+Route::resource('incidencias', IncidenciaController::class)->names([
+    'index' => 'incidencias.index',
+    'create' => 'incidencias.create',
+    'store' => 'incidencias.store',
+    'show' => 'incidencias.show',
+    'edit' => 'incidencias.edit',
+    'update' => 'incidencias.update',
+    'destroy' => 'incidencias.destroy',
+]);
+
+Route::resource('asistentes', AsistenteController::class)->names([
+    'index' => 'asistentes.index',
+    'create' => 'asistentes.create',
+    'store' => 'asistentes.store',
+    'show' => 'asistentes.show',
+    'edit' => 'asistentes.edit',
+    'update' => 'asistentes.update',
+    'destroy' => 'asistentes.destroy',
+]);
+
+Route::resource('tiposeventos', TipoEventoController::class)->names([
+    'index' => 'tiposeventos.index',
+    'create' => 'tiposeventos.create',
+    'store' => 'tiposeventos.store',
+    'show' => 'tiposeventos.show',
+    'edit' => 'tiposeventos.edit',
+    'update' => 'tiposeventos.update',
+    'destroy' => 'tiposeventos.destroy',
+]);
+
+Route::resource('acta', ActaController::class)->names([
+    'index' => 'acta.index',
+    'create' => 'acta.create',
+    'store' => 'acta.store',
+    'show' => 'acta.show',
+    'edit' => 'acta.edit',
+    'update' => 'acta.update',
+    'destroy' => 'acta.destroy',
+]);
+
+Route::resource('memorandum', MemorandumController::class)->names([
+    'index' => 'memorandum.index',
+    'create' => 'memorandum.create',
+    'store' => 'memorandum.store',
+    'show' => 'memorandum.show',
+    'edit' => 'memorandum.edit',
+    'update' => 'memorandum.update',
+    'destroy' => 'memorandum.destroy',
+]);
+
+Route::resource('evento', EventController::class)->names([
+    'index' => 'evento.index',
+    'create' => 'evento.create',
+    'store' => 'evento.store',
+    'show' => 'evento.show',
+    'edit' => 'evento.edit',
+    'update' => 'evento.update',
+    'destroy' => 'evento.destroy',
+]);
 // rama populate
 //! pop rama
 //LordAndre
