a45069708d49f36410acd87f0af72889f32d553b
Joelgomez <ioelgomez2019@gmail.com>
2023-07-17 00:07:16 -0500

    chipil 1

--- resources/views/livewire/memoranda/memolist.blade.php
+++ resources/views/livewire/memoranda/memolist.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @if($memos->count())
     <div class="row">
         <div class="col-12">
             <div class="input-group mb-3">
@@ -8,32 +9,36 @@
             </div>
         </div>
     </div>
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th scope="col">N°</th>
-                <th scope="col">Número</th>
-                <th scope="col">Año</th>
-                <th scope="col">Remitente</th>
-                <th scope="col">Asunto</th>
-                <th scope="col">Cuerpo</th>
-                <th scope="col">Acción</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($memos as $memo)
+
+        <table class="table table-striped table-hover">
+            <thead>
                 <tr>
-                    <td>{{ $memo->id }}</td>
-                    <td>{{ $memo->numero_memorandum }}</td>
-                    <td>{{ $memo->anio_memorandum }}</td>
-                    <td>{{ $memo->remitente_memorandum }}</td>
-                    <td>{{ $memo->nombre_tipo_evento }}</td>
-                    <td>{{ $memo->cuerpo_memorandum }}</td>
-                    <td>xd</td>
+                    <th scope="col">N°</th>
+                    <th scope="col">Número</th>
+                    <th scope="col">Año</th>
+                    <th scope="col">Remitente</th>
+                    <th scope="col">Asunto</th>
+                    <th scope="col">Cuerpo</th>
+                    <th scope="col">Acción</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($memos as $memo)
+                    <tr>
+                        <td>{{ $memo->id }}</td>
+                        <td>{{ $memo->numero_memorandum }}</td>
+                        <td>{{ $memo->anio_memorandum }}</td>
+                        <td>{{ $memo->remitente_memorandum }}</td>
+                        <td>{{ $memo->nombre_tipo_evento }}</td>
+                        <td>{{ $memo->cuerpo_memorandum }}</td>
+                        <td><button><i class="fa-solid fa-eye"></i></button>
+                            <button id="miBoton"> <i class="fa-solid fa-pen-to-square"></i></button>
+                            <button id="miBoton2"> <i class="fa-solid fa-trash"></i></button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-end">
             <!-- Botón "Anterior" -->
@@ -90,4 +95,27 @@
             </li>
         </ul>
     </nav>
+    @else
+        <div class="px-12 py-1">
+            No existe ningun Memorandum
+        </div>
+
+    @endif
 </div>
+<script>
+// Obtener el botón por su ID
+    var boton = document.getElementById("miBoton");
+    var boton2 = document.getElementById("miBoton2");
+
+
+    // Agregar un evento de clic al botón
+    boton.addEventListener("click", function() {
+
+    // Realizar alguna acción al hacer clic en el botón
+    console.log("¡Haz hecho clic en el botón editar!");
+    });
+    boton2.addEventListener("click", function() {
+    // Realizar alguna acción al hacer clic en el botón
+    console.log("¡Haz hecho clic en el botón eliminar!");
+    });
+</script>
